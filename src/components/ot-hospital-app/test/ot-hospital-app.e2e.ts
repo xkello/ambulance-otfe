ef7bf183ff9e1b978925d98a8fb03bb3a0096d3d
Richard Kello <xkello@stuba.sk>
2025-06-09 23:41:28 +0200

    typo

--- src/components/ot-hospital-app/test/ot-hospital-app.e2e.ts
+++ src/components/ot-hospital-app/test/ot-hospital-app.e2e.ts
@@ -3,7 +3,7 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('ot-hospital-app', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('ot-hospital-app></ot-hospital-app>');
+    await page.setContent('<ot-hospital-app></ot-hospital-app>');
 
     const element = await page.find('ot-hospital-app');
     expect(element).toHaveClass('hydrated');
